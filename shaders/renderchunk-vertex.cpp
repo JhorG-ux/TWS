9e1117940c7939a45606946b052a8b4de0d9a240
PROPHESSOR <xenteze@gmail.com>
2017-11-20 14:54:37 +0200

    Начал определять зависимости

--- shaders/renderchunk-vertex.cpp
+++ shaders/renderchunk-vertex.cpp
@@ -5,7 +5,7 @@
 #define UNDER_WATER
 #define WATER_WAVES
 #define PLANT_WAVES
-//#define ADVANCED_PLANT_FILTER
+#define ADVANCED_PLANT_FILTER
 
 //Тестирование
 //#define TEST
@@ -141,10 +141,14 @@ void main() {
    				float index = floor(texcoord.x) + floor(texcoord.y) * 32.0;
 				float power = 0.0;
 				if(index >= 151.0 && index <= 169.0){
+					//Спавнер
+					//Красный цветок
+					//Семена арбуза
  					power = 0.9;
 				}else if(index == 180.0 || index == 181.0){
+					//Ничего
 					power = 0.75;
-				}else if(index == 262.0 || index == 263.0){
+				}/*else if(index == 262.0 || index == 263.0){
 					power = 0.45;
 				}else if(index >= 64.0 && index <= 96.0 || index >= 133.0 && index < 137.0 || index >= 206.0 && index <= 213.0 || index >= 224.0 && index <= 226.0){
 					power = 1.0;
@@ -161,9 +165,7 @@ void main() {
 					}
 				}else if(index == 175.0){
 					power = 0.3;
-				}else if(index == 206.0){
-					power = 0.5;
-				}
+				}*/
 				/*switch(index){
 					default:
 						float power = 0.05;
